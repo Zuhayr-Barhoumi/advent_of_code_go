770e400264e8c79b61d633e2e1465f4178ad1794
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2026-05-06 19:35:14 +0100

    day3 first attempt solved

---
+++ 2015/day03/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type House struct {
+	X, Y int
+}
+
+type Grid[T any] struct {
+	cells map[House]T
+}
+
+func NewGrid[T any]() *Grid[T] {
+	return &Grid[T]{
+		cells: make(map[House]T),
+	}
+}
+
+// Set: Stores a value at x, y
+func (g *Grid[T]) Set(x, y int, value T) {
+	g.cells[House{X: x, Y: y}] = value
+}
+
+// Get: Retrievs a value at x, y
+// Returns the value and a boolean indicating if the cell exists (is not empty)
+func (g *Grid[T]) Get(x, y int) (T, bool) {
+	val, ok := g.cells[House{X: x, Y: y}]
+	return val, ok
+}
+
+// Len: returns the number of cells that currently hold a value
+func (g *Grid[T]) Len() int {
+	return len(g.cells)
+}
+
+func markHouse(location House, grid Grid[int]) {
+	if val, ok := grid.Get(location.X, location.Y); ok {
+		val++
+	} else {
+		grid.Set(location.X, location.Y, 1)
+	}
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	location := House{X: 0, Y: 0}
+	grid := NewGrid[int]()
+
+	// Set the initial house as visited once
+	grid.Set(location.X, location.Y, 1)
+
+	moves := strings.Split(strings.TrimSpace(string(data)), "")
+	for _, char := range moves {
+		switch char {
+		case "^":
+			fmt.Printf("%s: Up\n", char)
+			location.Y++
+			markHouse(location, *grid)
+		case ">":
+			fmt.Printf("%s: Right\n", char)
+			location.X++
+			markHouse(location, *grid)
+		case "<":
+			fmt.Printf("%s: Left\n", char)
+			location.X--
+			markHouse(location, *grid)
+		case "v":
+			fmt.Printf("%s: Down\n", char)
+			location.Y--
+			markHouse(location, *grid)
+		default:
+			fmt.Printf("%s: Other\n", char)
+		}
+	}
+	numberOfHousesVisited := grid.Len()
+	fmt.Printf("Number of houses that received at least one gift: %d \n", numberOfHousesVisited)
+	// 2572
+}
---
+++ 2015/day03/part1.go
@@ -0,0 +1,14 @@
+package main
+
+func part1() {
+	/* Create a 2D grid
+	 * Set the starting location as visited (0, 0)
+	 * Take the next move:
+		* north (^) add 1 to row which is the first value,
+		* south (v) subtract 1 from the row,
+		* east (>) add 1 to the column which is the second value,
+		* west subtract 1 from the column(<)
+		* After any of those moves mark the cell as visited once more
+	*/
+
+}
