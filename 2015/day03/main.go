673baab6b669894c58fcd8b9867d1fb814fdbe22
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2026-05-06 20:21:24 +0100

    now actually splitting into parts of day3

--- 2015/day03/main.go
+++ 2015/day03/main.go
@@ -59,32 +59,6 @@ func main() {
 	}
 	moves := strings.Split(strings.TrimSpace(string(data)), "")
 
-	location := House{X: 0, Y: 0}
-	grid := NewGrid[int]()
+	fmt.Printf("Number of houses that received at least one gift: %d \n", part1(moves))
 
-	// Set the initial house gifts to 1
-	grid.Set(location.X, location.Y, 1)
-
-	for _, char := range moves {
-		switch char {
-		case "^":
-			location.Y++
-			markHouse(location, grid)
-		case ">":
-			location.X++
-			markHouse(location, grid)
-		case "<":
-			location.X--
-			markHouse(location, grid)
-		case "v":
-			location.Y--
-			markHouse(location, grid)
-		}
-	}
-	numberOfHousesVisited := grid.Len()
-	fmt.Printf("Number of houses that received at least one gift: %d \n", numberOfHousesVisited)
-	// 2572
-	// for house, val := range grid.cells {
-	// 	fmt.Printf(" —— House: %d, %d —— Gifts: %d\n", house.X, house.Y, val)
-	// }
 }
--- 2015/day03/part1.go
+++ 2015/day03/part1.go
@@ -1,6 +1,6 @@
 package main
 
-func part1() {
+func part1(moves []string) int {
 	/* Create a 2D grid
 	 * Set the starting location as visited (0, 0)
 	 * Take the next move:
@@ -10,5 +10,36 @@ func part1() {
 		* west subtract 1 from the column(<)
 		* After any of those moves mark the cell as visited once more
 	*/
+
+	location := House{X: 0, Y: 0}
+	grid := NewGrid[int]()
+
+	// Set the initial house gifts to 1
+	grid.Set(location.X, location.Y, 1)
+
+	for _, char := range moves {
+		switch char {
+		case "^":
+			location.Y++
+			markHouse(location, grid)
+		case ">":
+			location.X++
+			markHouse(location, grid)
+		case "<":
+			location.X--
+			markHouse(location, grid)
+		case "v":
+			location.Y--
+			markHouse(location, grid)
+		}
+	}
+	numberOfHousesVisited := grid.Len()
+	return numberOfHousesVisited
+	// fmt.Printf("Number of houses that received at least one gift: %d \n", numberOfHousesVisited)
+
+	// 2572
+	// for house, val := range grid.cells {
+	// 	fmt.Printf(" —— House: %d, %d —— Gifts: %d\n", house.X, house.Y, val)
+	// }
 
 }
