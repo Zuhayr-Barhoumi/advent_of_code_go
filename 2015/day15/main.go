442dcd154a57e7778db68e4a2a9ee6fc2c71f339
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2026-05-15 22:19:22 +0100

    day15 part1 solved

--- 2015/day15/main.go
+++ 2015/day15/main.go
@@ -2,16 +2,87 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type ingredient struct {
+	name                                            string
 	capacity, durability, flavor, texture, calories int
 }
 
+func getIngredients(lines []string) []ingredient {
+	ingredients := []ingredient{}
+
+	for _, line := range lines {
+		words := strings.Fields(line)
+		name := strings.TrimSuffix(string(words[0]), ":")
+		capacity, _ := strconv.Atoi(strings.TrimSuffix(words[2], ","))
+		durability, _ := strconv.Atoi(strings.TrimSuffix(words[4], ","))
+		flavor, _ := strconv.Atoi(strings.TrimSuffix(words[6], ","))
+		texture, _ := strconv.Atoi(strings.TrimSuffix(words[8], ","))
+		calories, _ := strconv.Atoi(strings.TrimSuffix(words[10], ","))
+
+		ing := ingredient{
+			name,
+			capacity,
+			durability,
+			flavor,
+			texture,
+			calories,
+		}
+
+		ingredients = append(ingredients, ing)
+	}
+	return ingredients
+}
+
+func calcRecipeScore(ingredients []ingredient, recipe []int) int {
+	capacity := 0
+	durability := 0
+	flavor := 0
+	texture := 0
+
+	for i := range ingredients {
+		capacity += ingredients[i].capacity * recipe[i]
+		durability += ingredients[i].durability * recipe[i]
+		flavor += ingredients[i].flavor * recipe[i]
+		texture += ingredients[i].texture * recipe[i]
+	}
+
+	capacity = max(capacity, 0)
+	durability = max(durability, 0)
+	flavor = max(flavor, 0)
+	texture = max(texture, 0)
+
+	return capacity * durability * flavor * texture
+}
+
 func solve1(lines []string) int {
-	return -1
+	ingredients := getIngredients(lines)
+
+	recipeCombos := [][]int{}
+
+	for i := 0; i <= 100; i++ {
+		for j := 0; j <= 100-i; j++ {
+			for k := 0; k <= 100-i-j; k++ {
+				recipeCombos = append(recipeCombos, []int{i, j, k, 100 - i - j - k})
+			}
+		}
+	}
+
+	bestTotal := math.MinInt
+
+	for _, r := range recipeCombos {
+		total := calcRecipeScore(ingredients, r)
+		if total > bestTotal {
+			bestTotal = total
+		}
+	}
+
+	return bestTotal
 }
 
 func main() {
