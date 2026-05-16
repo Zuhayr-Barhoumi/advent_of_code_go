bb71245d9f2d5bdc01a0f57694b3271112c348f0
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2026-05-16 01:48:02 +0100

    day16 part1

--- 2015/day16/main.go
+++ 2015/day16/main.go
@@ -2,11 +2,69 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
+var tickerTape = map[string]int{
+	"children":    3,
+	"cats":        7,
+	"samoyeds":    2,
+	"pomeranians": 3,
+	"akitas":      0,
+	"vizslas":     0,
+	"goldfish":    5,
+	"trees":       3,
+	"cars":        2,
+	"perfumes":    1,
+}
+
+func getAunts(lines []string) []map[string]int {
+	sues := make([]map[string]int, 0)
+
+	for _, line := range lines {
+		words := strings.Fields(line)
+
+		prop1 := strings.TrimSuffix(words[2], ":")
+		val1, err := strconv.Atoi(strings.TrimSuffix(words[3], ","))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		prop2 := strings.TrimSuffix(words[4], ":")
+		val2, err := strconv.Atoi(strings.TrimSuffix(words[5], ","))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		prop3 := strings.TrimSuffix(words[6], ":")
+		val3, err := strconv.Atoi(strings.TrimSuffix(words[7], ","))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sues = append(sues, map[string]int{prop1: val1, prop2: val2, prop3: val3})
+	}
+
+	return sues
+}
+
 func solve1(lines []string) int {
+	sues := getAunts(lines)
+	for i, sue := range sues {
+		match := true
+		for prop, val := range sue {
+			if tickerTape[prop] != val {
+				match = false
+				break
+			}
+		}
+		if match {
+			return i + 1
+		}
+	}
 	return -1
 }
 
