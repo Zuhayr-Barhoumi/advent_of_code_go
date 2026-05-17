c959fdbdccad1e4d5b2d33d522e8fe90a68ff242
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2026-05-17 17:30:31 +0100

    day18 part1 init

--- 2015/day18/main.go
+++ 2015/day18/main.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+func solve1(lines []string) int {
+	return -1
+}
+
 func main() {
 	file, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -13,7 +17,7 @@ func main() {
 		return
 	}
 
-	line := strings.Split(strings.TrimSpace(string(file)), "\n")
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
 
-	fmt.Println("Part-1 — Number of on lights after 100 steps:", solve1(line))
+	fmt.Println("Part-1 — Number of on lights after 100 steps:", solve1(lines))
 }
